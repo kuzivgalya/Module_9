88855999bb2e8afcf2b9d8415f1d33423f8f988e
Halyna Kuziv <kuzivgm@gmail.com>
2022-10-14 10:37:43 +0300

    Update dz9.py

--- dz9.py
+++ dz9.py
@@ -2,55 +2,57 @@ CONTACTS = {}
 
 
 def input_error(handler):
-
     def wrapper(*args, **kwargs):
         try:
             handler(*args, **kwargs)
         except ValueError:
             print('Uncorrect number')
+            return ''
+        except IndexError:
+            print('Give me name and phone please')
+            return ''
         except KeyError:
             print('No such contact')
-        except IndexError:
-            print('Enter more information')
+            return ''
+        else:
+            return handler(*args, **kwargs)
     return wrapper
-
-
-def start_session():
-    print('How can I help you?')
 
 
 @input_error
 def add_contact(command):
     words = command.split()
-    name = words[1]
-    phone = words[2]
+    name = words[1].lower()
+    phone = int(words[2])
     CONTACTS[name] = phone
+    return 'Contact is added'
 
 
 @input_error
 def change_contact(command):
     words = command.split()
-    name = words[1]
+    name = words[1].lower()
     if name in CONTACTS:
-        phone = words[2]
-        CONTACTS[name] = phone
+        CONTACTS[name] = int(words[2])
     else:
-        print('No such name in your contacts')
-    
+        return 'No such name in your contacts'
+    return 'Contact is changed'
+        
 
 @input_error
 def find_contact(command):
     words = command.split()
-    name = words[1]
-    print(name, CONTACTS[name])
+    name = words[1].lower()
+    return CONTACTS[name]
 
 
 def show_all_contacts():
     if CONTACTS == {}:
-        print('No contacts yet')
+        return 'No contacts yet'
     else:
         for key, value in CONTACTS.items():
             print(key, value)
+        return ''
 
 
 def main():
@@ -58,17 +60,18 @@ def main():
     while True:
         user_command = input("Enter command: ")
         words = user_command.split()
-        if words[0] == 'hello':
-            start_session()
-        elif words[0] == 'add':
-            add_contact(user_command)
-        elif words[0] == 'change':
-            change_contact(user_command)
-        elif words[0] == 'phone':
-            find_contact(user_command)
-        elif words[0] == 'show':
-            show_all_contacts()
-        elif words[0] in ['good', 'close', 'exit']:
+        command = words[0].lower()
+        if command == 'hello':
+            print('How can I help you?')
+        elif command == 'add':
+            print(add_contact(user_command))
+        elif command == 'change':
+            print(change_contact(user_command))
+        elif command == 'phone':
+            print(find_contact(user_command))
+        elif command == 'show':    
+            print(show_all_contacts())
+        elif command in ['good', 'close', 'exit']:
             break
         else:
             print('Wrong command')
@@ -78,4 +81,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
